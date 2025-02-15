638317533340d953132212a66287ca14dfc968a6
UBC Student <lam.michael.81201@gmail.com>
2025-02-14 21:54:47 -0800

    continued work on text and list

--- newsletter-signup-form/src/App.jsx
+++ newsletter-signup-form/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <>
       {!submitted ? <MainComponent /> : <SubmittedComponent />}
+      <p className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/michlam" target="_blank">Michael Lam</a>.
+      </p>
     </>
   )
 }
