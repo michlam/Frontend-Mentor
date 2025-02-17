1bbf7d4006ebb116d40899db2913c7545047cffb
UBC Student <lam.michael.81201@gmail.com>
2025-02-16 20:50:59 -0800

    Setup basic structure and footer

---
+++ time-tracking-dashboard/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/michlam" target="_blank">Michael Lam</a>.
+        </div>
+    )
+} 
