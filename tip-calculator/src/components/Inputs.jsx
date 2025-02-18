208beae344977a24f82ee192b74c01a4f7a62ec5
UBC Student <lam.michael.81201@gmail.com>
2025-02-18 15:46:55 -0800

    Completed bill container and styling

--- tip-calculator/src/components/Inputs.jsx
+++ tip-calculator/src/components/Inputs.jsx
@@ -1,9 +1,29 @@
-export default function Inputs() {
+// The inputs component where most of the user inputs will be handled.
+
+export default function Inputs(props) {
     return (
         <div className="inputs">
-            
-            Inputs go here
-            
+            <div className="bill-container">
+                <label htmlFor="billInput">Bill</label>
+                <input name="billInput" />
+            </div>
+
+            <div className="tip-container">
+                <label>Select Tip%</label>
+                <div className="tip-buttons">
+                    <button>5%</button>
+                    <button>10%</button>
+                    <button>15%</button>
+                    <button>25%</button>
+                    <button>50%</button>
+                    <input />
+                </div>
+            </div>
+
+            <div className="people-num-container">
+                <label htmlFor="peopleNum">Number of people</label>
+                <input name="peopleNum" />
+            </div>
         </div>
     )
 }
--- tip-calculator/src/components/Main.jsx
+++ tip-calculator/src/components/Main.jsx
@@ -1,7 +1,12 @@
 import Inputs from "./Inputs"
 import Display from "./Display"
+import { useState } from "react"
 
 export default function Main() {
+    const [bill, setBill] = useState(0);
+    const [tip, setTip] = useState(0);
+    const [peopleNum, setPeopleNum] = useState(0);
+
     return (
         <main>
             <Inputs />
