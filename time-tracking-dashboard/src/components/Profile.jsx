0aaed4b9e492a78cd3e7644e215915e7f6e957b0
UBC Student <lam.michael.81201@gmail.com>
2025-02-17 21:04:50 -0800

    Completed all desktop design and responsiveness

--- time-tracking-dashboard/src/components/Profile.jsx
+++ time-tracking-dashboard/src/components/Profile.jsx
@@ -1,7 +1,42 @@
+import Avatar from "../assets/images/image-jeremy.png"
+
 export default function Profile(props) {
+    const activeStyle = {
+        color: "white"
+    }
+
     return (
         <section className="profile">
-            This is the profile section
+            <div className="profile-top">
+                <img src={Avatar} />
+                <p>Report for</p>
+                <h1>Jeremy Robson</h1>
+            </div>
+
+            <div className="profile-bot">
+                <h3 
+                    style={props.time == "Daily" ? activeStyle : null} 
+                    onClick={() => props.changeTime("Daily")}
+                >
+                    Daily
+                </h3>
+
+                <h3 
+                    style={props.time == "Weekly" ? activeStyle : null}
+                    onClick={() => props.changeTime("Weekly")}
+                >
+                    Weekly
+                </h3>
+                
+                <h3 
+                    style={props.time == "Monthly" ? activeStyle : null}
+                    onClick={() => props.changeTime("Monthly")}    
+                >
+                    Monthly
+                </h3>
+
+            </div>
+
         </section>
     )
 }
--- time-tracking-dashboard/src/components/Timecard.jsx
+++ time-tracking-dashboard/src/components/Timecard.jsx
@@ -4,19 +4,30 @@ import SelfcareIcon from "../assets/images/icon-self-care.svg"
 import SocialIcon from "../assets/images/icon-social.svg"
 import StudyIcon from "../assets/images/icon-study.svg"
 import WorkIcon from "../assets/images/icon-work.svg"
+import EllipsesIcon from "../assets/images/icon-ellipsis.svg"
+import { useEffect, useState } from "react"
 
 export default function Timecard(props) {
     const propsId = props.id.split(" ").join("-").toLowerCase();
     const bgStyle = {
         backgroundColor: props.colour,
     }
-
+    
     return (
         <section 
             className={`timecard ${propsId}`} 
             style={bgStyle}
         >
-            This is a timecard
+            <div className="timecard-content">
+                <div className="tc-top">
+                    <h3>{props.id}</h3>
+                    <img src={EllipsesIcon} />
+                </div>
+
+                <h1>{props.currTimeNum}hrs</h1>
+                <p>Last {props.timeword} - {props.prevTimeNum}hrs</p>
+                
+            </div>
         </section>
     )
 }
