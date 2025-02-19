f9796626e3debcd813a9884fddd05ec50d758b3f
UBC Student <lam.michael.81201@gmail.com>
2025-02-18 16:30:01 -0800

    completed display section and styling

--- tip-calculator/src/components/Display.jsx
+++ tip-calculator/src/components/Display.jsx
@@ -1,7 +1,26 @@
 export default function Display() {
     return (
         <div className="display">
-            Display goes here
+            <div className="tip-amount-container">
+                <div className="tip-amount-left">
+                    <h4>Tip Amount</h4>
+                    <p>/ person</p>
+                </div>
+
+                <h1>$0.00</h1>
+            </div>
+
+            <div className="total-container">
+                <div className="total-left">
+                    <h4>Total</h4>
+                    <p>/ person</p>
+                </div>
+
+                <h1>$0.00</h1>
+            </div>
+
+            <button>RESET</button>
+
         </div>
     )
 }
