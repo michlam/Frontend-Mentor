31e8e09b40df03c82d1feb22b4c79c7a50e846ba
UBC Student <lam.michael.81201@gmail.com>
2025-02-20 18:59:54 -0800

    Styling for main component

--- interactive-rating-component/src/App.jsx
+++ interactive-rating-component/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 import Footer from "./components/Footer.jsx"
+import Main from "./components/Main.jsx"
+import Thanks from "./components/Thanks.jsx"
+import {useState} from "react"
 
 function App() {
+  const [submitted, setSubmitted] = useState(false);
+  const [rating, setRating] = useState(null);
+
   return (
     <>
+      {!submitted ? <Main /> : <Thanks/>}
       <Footer />
     </>
   )
